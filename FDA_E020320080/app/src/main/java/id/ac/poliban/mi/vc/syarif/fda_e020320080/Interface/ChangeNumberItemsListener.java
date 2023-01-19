182aa8a9700072f1d77d4a9378fcc8a0988acbb7
Ryashadiq <shadiqryas@gmail.com>
2023-01-19 21:17:51 +0700

    first commit

---
+++ FDA_E020320080/app/src/main/java/id/ac/poliban/mi/vc/syarif/fda_e020320080/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vc.syarif.fda_e020320080.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ Project82_E020320080/app/src/main/java/id/ac/poliban/mi/vc/syarif/project82_e020320080/LoginActivity.java
@@ -0,0 +1,13 @@
+package id.ac.poliban.mi.vc.syarif.project82_e020320080;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
